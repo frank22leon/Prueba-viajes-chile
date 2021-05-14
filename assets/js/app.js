8df8deef51f0e7348ff2f50368f48bf7314b74ba
Francisco leon <22012000fdlq@gmail.com>
2021-05-13 20:13:56 -0400

    terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+   
+        var altura = $('.menu').offset().top;
+        
+        $(window).on('scroll', function(){
+            if ( $(window).scrollTop()  > 40){
+                $('.menu').addClass('bg-info').removeClass('bg-transparent');
+            } else {
+                $('.menu').addClass('bg-transparent').removeClass('bg-info');
+            }
+        });
+    
+  
+    
     $('[data-toggle="popover"]').popover()
 
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-          window.location.hash = hash;
-        });
-      } 
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
     });
-  });
- 
+});
+
+
